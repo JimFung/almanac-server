e0bf61a66540da650607a3dfb41db70d7c4f6d69
JimFung <jfung92@gmail.com>
2017-05-02 20:48:07 -0700

    updated to use env vars instead of a config

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const { dbAddr, PORT, log_level } = require('./config')
 const fs = require('fs')
 
 //Setting up DB settings
 mongoose.Promise = global.Promise
-mongoose.connect(dbAddr)
+mongoose.connect(process.env.ALMANAC_DB)
 const db = mongoose.connection
 
 db.on('error', (err) =>{
@@ -18,15 +17,16 @@ db.once('open', () => {
   console.log('Connected to mongodb')
 })
 
+const log_fmt = ':date[web] :method :url :status :response-time ms'
 //Setting up logging
 let log_stream = fs.createWriteStream(`${new Date().toDateString()}.log`, {flags: 'a'})
-app.use(morgan(':date[web] :method :url :status :response-time ms', {stream: log_stream})) //log to file
-app.use(morgan(log_level)) //log to terminal
+app.use(morgan(log_fmt, {stream: log_stream})) //log to file
+app.use(morgan(log_fmt)) //log to terminal
 
 //Setting up express routes
 require('./routes')(app, mongoose)
 
 //Starting the server
-app.listen(PORT, () => {
-  console.log(`Server running at ${PORT}`)
+app.listen(process.env.ALMANAC_PORT, () => {
+  console.log(`Server running at ${process.env.ALMANAC_PORT}`)
 })
